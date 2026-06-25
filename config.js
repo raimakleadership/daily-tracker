a55790cd1cfad1889d4c56851afee906b0e6b21a
raimakleadership <B.Hinesley@raimak.com>
2026-06-25 15:05:47 -0400

    Created config.js for agent selection/assignment

---
+++ config.js
@@ -0,0 +1,80 @@
+const agentDirectory = [
+  // --- REMOTE SALES CENTER ---
+  { name: "Caressa Scarrett", team: "Remote", mode: "sc" },
+  { name: "Meghann Stevens", team: "Remote", mode: "sc" },
+  { name: "Ken Roundtree", team: "Remote", mode: "sc" },
+  { name: "Kelly Moats", team: "Remote", mode: "sc" },
+
+  // --- FLORIDA SALES CENTER ---
+  { name: "Noah Caldwell", team: "Florida", mode: "sc" },
+  { name: "Kenny Leroux", team: "Florida", mode: "sc" },
+  { name: "Angelica", team: "Florida", mode: "sc" },
+  { name: "Michael McAlpine", team: "Florida", mode: "sc" },
+  { name: "Julian Torres", team: "Florida", mode: "sc" },
+
+  // --- TEXAS SALES CENTER ---
+  { name: "Jon Scroggins", team: "Texas", mode: "sc" },
+  { name: "Nate Jones", team: "Texas", mode: "sc" },
+  { name: "Everett Henry", team: "Texas", mode: "sc" },
+  { name: "Tory Mathis", team: "Texas", mode: "sc" },
+
+  // --- ADMIN ---
+  { name: "Alton Anderson", team: "Admin", mode: "admin" },
+  { name: "Brianna Woodall", team: "Admin", mode: "admin" },
+  { name: "Brittany Roman", team: "Admin", mode: "admin" },
+  { name: "Celinda Milland", team: "Admin", mode: "admin" },
+  { name: "Chelsey Foor", team: "Admin", mode: "admin" },
+  { name: "Emily Bou", team: "Admin", mode: "admin" },
+  { name: "Jessica Smith", team: "Admin", mode: "admin" },
+  { name: "Justin Felix", team: "Admin", mode: "admin" },
+  { name: "Krystyna Hinesley", team: "Admin", mode: "admin" },
+  { name: "Laura Saunders", team: "Admin", mode: "admin" },
+  { name: "Mackenzi Windle", team: "Admin", mode: "admin" },
+  { name: "Madeline Galeana", team: "Admin", mode: "admin" },
+  { name: "Makayla Elliot", team: "Admin", mode: "admin" },
+  { name: "Michelle Bunt", team: "Admin", mode: "admin" },
+  { name: "Misty Wheaton", team: "Admin", mode: "admin" },
+  { name: "Owen Bowden", team: "Admin", mode: "admin" },
+  { name: "Salvador Garcia", team: "Admin", mode: "admin" },
+  { name: "Sylvia Polanco", team: "Admin", mode: "admin" },
+  { name: "Tamieka Henry", team: "Admin", mode: "admin" },
+
+  // --- D2D: TY HUGHES ---
+  { name: "Donald Wiley", team: "Ty Hughes", mode: "d2d" },
+  { name: "Josh Urley", team: "Ty Hughes", mode: "d2d" },
+  { name: "Randy Robinson", team: "Ty Hughes", mode: "d2d" },
+  { name: "Austin Edminster", team: "Ty Hughes", mode: "d2d" },
+  { name: "Justus Redix", team: "Ty Hughes", mode: "d2d" },
+
+  // --- D2D: HOUSTON BRYANT ---
+  { name: "Michale McGee", team: "Houston Bryant", mode: "d2d" },
+  { name: "Stephon Davis", team: "Houston Bryant", mode: "d2d" },
+  { name: "Shawn Patrick", team: "Houston Bryant", mode: "d2d" },
+  { name: "Martin Garcia", team: "Houston Bryant", mode: "d2d" },
+  { name: "Lloyd Gregg", team: "Houston Bryant", mode: "d2d" },
+  { name: "Jason Lerma", team: "Houston Bryant", mode: "d2d" },
+
+  // --- D2D: INDEPENDENT CONTRACTORS ---
+  { name: "Shannon Kester", team: "Independent Contractor", mode: "d2d" },
+  { name: "Chris Miller", team: "Independent Contractor", mode: "d2d" },
+  { name: "Travis Goosens", team: "Independent Contractor", mode: "d2d" },
+
+  // --- KINETIC: JATON'S TEAM ---
+  { name: "Janice Pye", team: "Jaton's Team", mode: "kinetic" },
+  { name: "Gregg Henderson", team: "Jaton's Team", mode: "kinetic" },
+  { name: "Nicole Lawrence", team: "Jaton's Team", mode: "kinetic" },
+  { name: "Amanda Reynolds", team: "Jaton's Team", mode: "kinetic" },
+  { name: "Breanna Arrowood", team: "Jaton's Team", mode: "kinetic" },
+  { name: "Tyler Garrett", team: "Jaton's Team", mode: "kinetic" },
+  { name: "Isabel Turrubiartez", team: "Jaton's Team", mode: "kinetic" },
+  { name: "Foster Barrett", team: "Jaton's Team", mode: "kinetic" },
+
+  // --- KINETIC: ANDREW IRIZARRY'S TEAM ---
+  { name: "Jeffrey Nwokorie", team: "Andrew Irizarry's Team", mode: "kinetic" },
+  { name: "Skyler Westervelt", team: "Andrew Irizarry's Team", mode: "kinetic" },
+  { name: "Rodolfo Banda", team: "Andrew Irizarry's Team", mode: "kinetic" },
+  { name: "Damian Gatling", team: "Andrew Irizarry's Team", mode: "kinetic" },
+
+  // --- KINETIC: INDEPENDENT CONTRACTORS ---
+  { name: "Marketing Contractors", team: "Independent Contractor", mode: "kinetic" }
+];
